057035dc34aebe3ec097e9f7ef7a2e1aaf25432b
Jatin <contact.dev.jatin@gmail.com>
2024-02-22 17:10:31 +0530

    feat: about section responsive

--- lib/views/dashboard/views/about_view/tablet.dart
+++ lib/views/dashboard/views/about_view/tablet.dart
@@ -10,9 +10,73 @@ class _AboutViewTablet extends StatelessWidget {
       height: Get.height - Dimens.navbarHeight,
       child: Padding(
         padding: ResponsiveState.tablet.pagePadding,
-        child: const Column(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [],
+          children: [
+            const AboutImageRow(small: true),
+            Dimens.boxHeight32,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Flexible(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              AppText(
+                                'Who am I?',
+                                style: context.textTheme.headlineMedium?.withTitleColor,
+                              ),
+                              Dimens.boxHeight8,
+                              const SkillRow(),
+                              Dimens.boxHeight16,
+                              AppText(
+                                StringConstants.aboutMe,
+                                style: context.textTheme.titleMedium?.withBodyColor,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Dimens.boxWidth32,
+                        Expanded(
+                          flex: 2,
+                          child: Image.asset(
+                            AssetConstants.cartoonFull,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Flexible(
+                        child: AppButton.outlined(
+                          label: 'Github',
+                          onTap: () {},
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Flexible(
+                        child: AppButton(
+                          label: 'LinkedIn',
+                          onTap: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                  Dimens.boxHeight50,
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/views/dashboard/widgets/my_photo.dart
+++ lib/views/dashboard/widgets/my_photo.dart
@@ -2,19 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:portfolio/res/res.dart';
 
 class ProfileImage extends StatelessWidget {
-  const ProfileImage({super.key});
+  const ProfileImage({
+    super.key,
+    this.small = false,
+  });
+
+  final bool small;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       key: const ValueKey('profile-image'),
-      height: AppConstants.profileImageSize,
-      width: AppConstants.profileImageSize,
+      height: AppConstants.profileImageSize(small),
+      width: AppConstants.profileImageSize(small),
       child: DecoratedBox(
         decoration: BoxDecoration(
           color: AppColors.primary.shade100,
           borderRadius: BorderRadius.circular(
-            AppConstants.profileImageSize,
+            AppConstants.profileImageSize(small),
           ),
           image: const DecorationImage(
             image: AssetImage(AssetConstants.cartoonDp),
