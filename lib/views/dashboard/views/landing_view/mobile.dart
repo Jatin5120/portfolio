12da53fdd2c72bb5fbb20970b4765b553eaa46c3
Jatin <contact.dev.jatin@gmail.com>
2024-02-29 20:35:25 +0530

    fix: small height mobile ui design

--- lib/views/dashboard/views/landing_view/mobile.dart
+++ lib/views/dashboard/views/landing_view/mobile.dart
@@ -5,48 +5,51 @@ class _LandingViewMobile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.maxFinite,
-      height: Get.height - Dimens.navbarHeight,
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: Get.width,
+        minHeight: Get.height - Dimens.navbarHeight,
+      ),
       child: Padding(
         padding: ResponsiveState.mobile.pagePadding,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Center(child: DashAnimation()),
-                  AppText(
-                    'Hey, I\'m',
-                    style: context.textTheme.titleMedium?.withBodyColor,
+            const SizedBox.shrink(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Center(child: DashAnimation()),
+                AppText(
+                  'Hey, I\'m',
+                  style: context.textTheme.titleMedium?.withBodyColor,
+                ),
+                FittedBox(
+                  child: AppText(
+                    'Jatin',
+                    style: GoogleFonts.getTextTheme(
+                      'Montserrat',
+                    ).displayLarge?.withTitleColor.copyWith(
+                          fontSize: 80,
+                          fontWeight: FontWeight.bold,
+                          height: 1,
+                          letterSpacing: 0,
+                        ),
                   ),
-                  FittedBox(
-                    child: AppText(
-                      'Jatin',
-                      style: GoogleFonts.getTextTheme(
-                        'Montserrat',
-                      ).displayLarge?.withTitleColor.copyWith(
-                            fontSize: 80,
-                            fontWeight: FontWeight.bold,
-                            height: 1,
-                            letterSpacing: 0,
-                          ),
-                    ),
-                  ),
-                  Dimens.boxHeight10,
-                  AppText(
-                    StringConstants.heroDescription,
-                    style: context.textTheme.bodyMedium?.withBodyColor,
-                  ),
-                  const SizedBox(height: 32),
-                  const HireMeButton(),
-                  const SizedBox(height: 16),
-                  const MyWorkButton(),
-                ],
-              ),
+                ),
+                Dimens.boxHeight10,
+                AppText(
+                  StringConstants.heroDescription,
+                  style: context.textTheme.bodyMedium?.withBodyColor,
+                ),
+                const SizedBox(height: 32),
+                const HireMeButton(),
+                const SizedBox(height: 16),
+                const MyWorkButton(),
+              ],
             ),
+            const SizedBox(height: 32),
             const SocialRow(center: true),
           ],
         ),
